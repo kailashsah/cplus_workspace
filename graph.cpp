015906ad470c1de32405ae4d48cf2d05b0b51cd1
kailashsah <kailashpati86@gmail.com>
2020-07-22 16:00:35 +0530

    dfs search (depth first search)

---
+++ graph.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <list>
+#include <iomanip>
+
+using namespace std;
+namespace dfs_search {
+
+	class Graph {
+
+	public:
+		Graph(int v);
+		void addEdge(int v, int w);
+		void dfs_traverse(int vertex, bool visited[]);
+
+	private:
+		int vertices = 0;
+		list<int> *adj;
+
+	};
+
+	Graph::Graph(int v) {
+		vertices = v;
+		adj = new list<int> [vertices];
+	}
+
+	void Graph::addEdge(int v, int w) {
+		adj[v].push_back(w);
+	}
+
+	void Graph::dfs_traverse(int vertex, bool visited[]) {
+		visited[vertex] = true;
+		cout << vertex << setw(4);
+
+		for (auto it = adj[vertex].begin(); it != adj[vertex].end(); it++) {
+			if (visited[*it] == false)
+				dfs_traverse(*it, visited);
+		}
+
+	}
+	int main() {
+
+		int size = 4;
+		Graph g(size);
+		bool *visited = new bool[size];
+		for (int i = 0; i < size; i++)
+			visited[i] = false;
+
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(1, 2);
+		g.addEdge(2, 0);
+		g.addEdge(2, 3);
+		g.addEdge(3, 3);
+		g.dfs_traverse(2, visited);  // output should be 2 0 1 3
+
+		return 0;
+	}
+
+}
+
+/*
+int main ()
+{
+	dfs_search::main();
+	return 0;
+}
+*/
+
