8cefcf2818b9c8e24b6b563fc7360f86f98d277e
kailash <kailashpati86@gmail.com>
2023-09-09 20:03:55 +0530

    algo

---
+++ ALGORITHM/MiscAlgo/src/easy/array_product.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+using namespace std;
+
+constexpr int fib(int n)
+{
+	return (n <= 1) ? n : fib(n - 2) + fib(n - 1);
+
+	// driver code
+	//cout << fib(5);
+}
---
+++ ALGORITHM/MiscAlgo/src/easy/find_smallest_in_array.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+using namespace std;
+
+int find_samllest(int arr[], int first, int last, int num)
+{
+
+	int mid = (first + last) / 2;
+	if (mid == last || mid == first)
+		return num;
+
+	if (arr[mid] == num)
+		return num;
+
+	if (arr[mid] > num)
+		num = find_samllest(arr, first, mid, num);
+	else if (arr[mid] < num)
+		num = find_samllest(arr, mid + 1, last, num);
+
+
+	return num;
+}
+void run_find_smallest_in_arr()
+{
+	//int arr[] = {4,5, 1, 2, 3};
+	int arr[] = { 1,2, 3, 4, 5 };
+	cout << find_samllest(arr, 0, 4, 4) << endl;
+}
