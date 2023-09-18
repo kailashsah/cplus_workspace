4925f0c10aca6ecab4d55f9ea892889310c8dd2d
kailash <kailashpati86@gmail.com>
2023-09-18 12:12:24 +0530

    reference_wrapper link

--- LANGUAGE/11/Misc11/src/reference_wrapper.cpp
+++ LANGUAGE/11/Misc11/src/reference_wrapper.cpp
@@ -7,6 +7,7 @@ using namespace std;
 	std::reference_wrapper<T> is a copyable and assignable object that imitates a reference (T&). It gives the non-nullable guarantee of a reference and the pointer-like flexibility to rebind to another object.
 
 	The usual way to create an std::reference_wrapper<T> is via std::ref (or std::cref for reference_wrapper<const T>)
+	https://www.nextptr.com/tutorial/ta1441164581/stdref-and-stdreference_wrapper-common-use-cases
 */
 class RefTest {
 public:
@@ -39,12 +40,10 @@ void run_reference_vector()
 	auto a = vec[0];
 	cout << to_string(a.get().i) << endl;
 }
-void run_reference_wrapper()
-{
-}
+
 
 //int main()
 //{
-//	run_reference_wrapper();
+//	run_reference_vector();
 //	return 0;	
 //}
--- LANGUAGE/17/Misc17/templ_integer_seq.cpp
+++ LANGUAGE/17/Misc17/templ_integer_seq.cpp
@@ -21,7 +21,7 @@ auto sum_(const std::tuple<ElementTypes ... >& tup, std::integer_sequence<int, I
 void run_integer_seq()
 {
 	tuple tup{ 1, 3.5, 2 };
-	cout << sum(tup); // becoz of 1 --  error C2338: duplicate type T in get<T>(tuple)
+	//cout << sum(tup); // becoz of 1 --  error C2338: duplicate type T in get<T>(tuple)
 
 	constexpr auto size = std::tuple_size_v<decltype(tup)>;
 	auto indices = std::make_integer_sequence<int, size>{};
