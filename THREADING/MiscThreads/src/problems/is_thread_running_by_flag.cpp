6f2118db6fcd9dc202311ca78a93e4b61b718900
kailash <kailashpati86@gmail.com>
2023-10-26 18:03:01 +0530

    is thread running

---
+++ THREADING/MiscThreads/src/problems/is_thread_running_by_flag.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+/*
+	1. check if( thread_done ) to evaluate thread running
+
+*/
+
+
+
+std::atomic<bool> thread_done = false;
+
+void* real_thread_function(void* ) {
+
+    return nullptr;
+}
+
+void* run_thread(void* arg)
+{
+    void* res = nullptr;
+
+    try
+    {
+        res = real_thread_function(arg);
+    }
+    catch (...)
+    {
+    }
+
+    thread_done = true;
+
+    return res;
+}
