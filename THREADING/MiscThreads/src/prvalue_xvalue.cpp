7c2d34c63b0e1faab3b3cb9bd80ca5544e0e9b3d
kailash <kailashpati86@gmail.com>
2023-11-02 12:35:35 +0530

    comment

--- THREADING/MiscThreads/src/prvalue_xvalue.cpp
+++ THREADING/MiscThreads/src/prvalue_xvalue.cpp
@@ -2,9 +2,12 @@
 using namespace std;
 
 /*
-	1. xvalue - An xvalue (an “eXpiring” value) also refers to an object, usually near the end of its lifetime (so that its resources may be moved, for example). An xvalue is the result of certain kinds of expressions involving rvalue references. [Example: The result of calling a function whose return type is an rvalue reference is an xvalue.]
+	1. xvalue - An xvalue (an “eXpiring” value) also refers to an object, usually near the end of its lifetime (so that its resources may be moved, for example). An xvalue is the result of certain kinds of expressions involving rvalue references. 
+		[Example: The result of calling a function whose return type is an rvalue reference is an xvalue.]
 
-	2. prvalue - A prvalue (“pure” rvalue) is an rvalue that is not an xvalue. [Example: The result of calling a function whose return type is not a reference is a prvalue].
+	2. prvalue (literal values) - A prvalue (“pure” rvalue) is an rvalue that is not an xvalue. 
+		[Example: The result of calling a function whose return type is not a reference is a prvalue].
+
 		 2.1 Example: The result of calling a function whose return type is not a reference is a prvalue. The value of a literal such as 12, 7.3e5, or true is also a prvalue
 
 	3. prvalues are the implicit moves, and xvalues are the explicit moves (std::move returns an xvalue).
