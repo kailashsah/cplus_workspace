05a1ce27b77abfdbd0bedb978ae66acfacebb83c
kailash <kailashpati86@gmail.com>
2023-11-02 12:35:14 +0530

    unique_lock

---
+++ THREADING/MiscThreads/src/cplus20/unique_lock_param_deduction.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*
+	1. std::unique_lock l(m);
+		c++17 allows template parameters to be deduced from constructors, removing the need for make_xxx(...) helper functions or explicitly specify typenames that the compiler should be able to deduce.
+		this is called Template function argument deduction.
+
+
+*/
+
+
---
+++ THREADING/MiscThreads/src/unique_lock.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/*
+	1. unique_lock - The class unique_lock is a general-purpose mutex ownership wrapper allowing deferred locking, time-constrained attempts at locking, recursive locking, transfer of lock ownership, and use with condition variables.
+
+	2.  use a unique lock when you want to conditionally lock or unlock a mutex, such as waiting for a condition variable or using a try_lock method.
+
+	3. std::unique_lock use the RAII pattern
+
+	4. If a exceptions is thrown, the std::unique_lock destructer will be called and so the mutex will be unlocked.
+
+*/
+
+
+
