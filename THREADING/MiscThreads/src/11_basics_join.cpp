4569e78666da06ff5b24b902b40d37d235d6a88e
kailash <kailashpati86@gmail.com>
2023-11-26 18:53:44 +0530

    renamed in order

--- THREADING/MiscThreads/src/11_basics_join.cpp
+++ THREADING/MiscThreads/src/11_basics_join.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
-
+/*
+		1. join() - The function returns when the thread execution has completed.
+			This synchronizes the moment this function returns with the completion of all the operations in the thread
+		
+		2. After a call to this function, the thread object becomes non-joinable and can be destroyed safely.
+		
+		3. when you join a thread that doesn't cause the thread to terminate. It simply blocks until that thread dies of natural (or unnatural) causes.
+*/
 #include <thread> // std::this_thread, thread class
 void pause_thread(int n)
 {
@@ -9,12 +16,7 @@ void pause_thread(int n)
 }
 
 void run_thread_join() {
-	/*
-		1. join() - The function returns when the thread execution has completed.
-			This synchronizes the moment this function returns with the completion of all the operations in the thread
-		2. After a call to this function, the thread object becomes non-joinable and can be destroyed safely.
-		3. when you join a thread that doesn't cause the thread to terminate. It simply blocks until that thread dies of natural (or unnatural) causes.
-	*/
+
 	std::cout << "Spawning 3 threads...\n";
 	std::thread t1(pause_thread, 1);
 	std::thread t2(pause_thread, 2);
@@ -26,8 +28,6 @@ void run_thread_join() {
 	std::cout << "All threads joined!\n";
 
 }
-
-
 
 //int main()
 //{
