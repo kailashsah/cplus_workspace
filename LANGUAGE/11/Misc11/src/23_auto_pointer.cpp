451de90a4fd10595d051611ed3ebc7cb5726f515
kailash <kailashpati86@gmail.com>
2024-01-02 16:27:56 +0530

    unique_ptr push_back() .. auto pointer

--- LANGUAGE/11/Misc11/src/23_auto_pointer.cpp
+++ LANGUAGE/11/Misc11/src/23_auto_pointer.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 /*
-	1. auto* 
+	1. auto*
 	2. auto - The auto keyword in C++ automatically detects and assigns a data type to the variable with which it is used. The compiler analyses the variable's data type by looking at its initialization.
 
-	3. https://www.scaler.com/topics/cpp/auto-in-cpp/ 
-		
+	3. https://www.scaler.com/topics/cpp/auto-in-cpp/
+
 */
 void run_auto_ptr_v1();
 void run_auto_references_v2();
@@ -21,7 +21,7 @@ void run_auto_ptr_v1() {
 	auto* p2 = &num2;
 
 	cout << "The values are " << *p1 << " and " << *p2 << endl;
-	
+
 }
 
 void run_auto_references_v2() {
@@ -37,7 +37,38 @@ void run_auto_references_v2() {
 
 	cout << "The value is " << x << endl;
 }
+void run_auto_ptr_usage() {
+	/*
+		1.
+			int i;
+			const auto* p = &i;
+			is equivalent to
 
+			int i;
+			const int* p = &i;  // read as 'pointer to const int'
+
+		2.
+			int i;
+			const auto p = &i;
+			is equivalent to
+
+			int i;
+			int* const p = &i; // read as "constant pointer to int"
+		
+		3. const int* const // constant pointer to const int 
+
+	*/
+	int a;
+	//1
+	const auto* p1 = &a;
+	//*p1 = 7;               // Error //'p1': you cannot assign to a variable that is const.
+	p1 = nullptr;          // OK
+
+	//2.
+	const auto p2 = &a;
+	*p2 = 7;               // OK
+	//p2 = nullptr;          // Error
+}
 //int main()
 //{
 //	run_auto_ptr_v1();
---
+++ LANGUAGE/11/Misc11/src/src_pointers/18_unique_ptr_vector_pushback.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#include <memory> // make_unique
+#include <algorithm>
+using namespace std;
+#include <vector>
+/*
+	1. Why can I not push_back a unique_ptr into a vector?
+*/
+void run_unique_ptr_pushback()
+{
+	std::vector<std::unique_ptr<int>> vec;
+
+	int x(1);
+	std::unique_ptr<int> ptr2x(&x);
+	//1.
+	//vec.push_back(ptr2x); //This tiny command has a vicious error.
+	//std::unique_ptr<int,std::default_delete<int>> &)': attempting to reference a deleted function	
+
+	/*
+		1. vec.push_back(std::move(ptr2x));
+		unique_ptr guarantees that a single unique_ptr container has ownership of the held pointer. This means that you can't make copies of a unique_ptr (because then two unique_ptrs would have ownership), so you can only move it.
+
+		2. std::unique_ptr has no copy constructor. You create an instance and then ask the std::vector to copy that instance during initialisation.
+	*/
+}
+
+//int main()
+//{
+//	run_unique_ptr_pushback();
+//	return 0;
+//}
