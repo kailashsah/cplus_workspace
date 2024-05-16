2bfd3b71d5aefc2bcd820595eda54cd52dbb9911
kailash <kailashpati86@gmail.com>
2024-05-16 18:28:06 +0530

    log writing using ofstream

---
+++ LANGUAGE/11/Misc11/src/101_write_log.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+#include <string>
+#include <fstream>
+#include <exception>
+#include <thread>
+
+
+void WriteDebugLog(string str)
+{
+
+	try {
+		ofstream out{ "Cef3BrowserLog.txt", ios_base::app };
+
+		out.end;
+		out << this_thread::get_id() << " " << str << "\n";
+		out.close();
+	}
+	catch (exception& e)
+	{
+
+
+	}
+}
+
+	//int main()
+	//{
+	//	run_version(); // c++98, always prints c++98 when changes to c++14 or c++17 
+	//	return 0;	
+	//}
