dcc46827155703427c487771bd2e91a9bc9f0ff1
kailash <kailashpati86@gmail.com>
2025-03-27 21:25:36 +0530

    format datetime specifier based on wcsftime.

--- LANGUAGE/windows_cplus/string_debugging/string_debugging.cpp
+++ LANGUAGE/windows_cplus/string_debugging/string_debugging.cpp
@@ -1,112 +1,128 @@
-// string_debugging.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include<string>
+using namespace std;
 //#define _CRT_SECURE_NO_WARNINGS // for localtime
+
+
+/*
+	1. this code is for test output purpose only.
+	2. It works on pointer movement that looks doubtful if like to refactor with smart pointers or string class.
+*/
 
 int FormatDateTimeSpecification(char* pIn, char* pOut, int nMaxLen);
 int main()
 {
-    std::cout << "Hello World!\n";
+	int  maxLen = 1024 + 2;
+	char szParameters[1024 + 2];
+
+
+	//string str= "#%m%d%Y#.dat";           // "03172025.dat"           # at both ends
+	//string str= "#%m%d%Y##.dat";          // "03172025#.dat"          adds # last if extra given
+	//string str= "#%m%d%Y##%m%d%Y#.dat";   // "0317202503172025.dat"   correct if # symmetry
+	//string str = "#%maaa%d%Y#.dat";        //03aaa182025.dat          mixed with char literals
+
+
+	//--FALSE CASES--
+	//string str = "#%m%d%Y#%m%d%Y#.dat";   //"03172025%m%d%Y#.dat"     if single # in between
+	//string str = "#%m%d%Y#%m%d%Y##%m%d%Y#.dat";   //03182025%m%d%Y#%m%d%Y#.dat     it shuld have  "#%m%d%Y# #%m%d%Y##%m%d%Y#.dat"
+	//string str= "#%m%d%Y.dat";            // "#%m%d%Y.dat"            # not given at last
+	//string str= "#%m'aaa'%d%Y.dat";       // #%m'aaa'%d%Y.dat         mixed with char literals with ' quotes
+	//string str= "#%m#.dat";       //03.dat
+	//string str= "##%m#";       //#%m#
+	//string str= "##%maada%d%y#";       // #%maadaa%d%y# 
+
+
+	FormatDateTimeSpecification((char*)str.data(), szParameters, maxLen);
+	std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
 int FormatDateTimeSpecification(char* pIn, char* pOut, int nMaxLen)
 {
-    time_t      Now;                        // Current DateTime
-    struct tm* pToday;                    // TimeDate Structure
-    int       nWork;                      // Work Value
-    char* pWork;                     // Work pointer
-    const char* pProbe;                    // Probe pointer
-    char* pFmt;                      // Format String pointer
-    int       nFmtLen;                    // Format Length
+	time_t Now;         // Current DateTime
+	struct tm* pToday;  // TimeDate Structure
+	int   nWork;        // Work Value
+	char* pWork;        // Work pointer
+	const char* pProbe; // Probe pointer
+	char* pFmt;         // Format String pointer
+	int   nFmtLen;      // Format Length
 
-    time(&Now);
-    pToday = localtime(&Now);
+	time(&Now);
+	pToday = localtime(&Now);
 
-    nMaxLen--;                              // Adjust for trailing \0
+	nMaxLen--;                              // Adjust for trailing \0
 
-    for (pWork = pOut; *pIn && nMaxLen > 0; pIn++)
-    {
-        /*
-        **+
-        **  First, check for non-pound or double pound.  Copy
-        **  them and continue.
-        **-
-        */
+	for (pWork = pOut; *pIn && nMaxLen > 0; pIn++)
+	{
+		/*
+		**+
+		**  First, check for non-pound or double pound.  Copy
+		**  them and continue.
+		**-
+		*/
 
-        if (*pIn != L'#')
-        {
-            *pWork++ = *pIn;
-            nMaxLen--;
-            continue;
-        }
+		if (*pIn != L'#')
+		{
+			*pWork++ = *pIn;
+			nMaxLen--;
+			continue;
+		}
 
-        pIn++;
-        if (*pIn == L'#')
-        {
-            *pWork++ = L'#';
-            nMaxLen--;
-            continue;
-        }
+		pIn++;
+		if (*pIn == L'#')
+		{
+			*pWork++ = L'#';
+			nMaxLen--;
+			continue;
+		}
 
-        /*
-        **+
-        **  This is a single-pound string.  Look for the ending pound.
-        **-
-        */
+		/*
+		**+
+		**  This is a single-pound string.  Look for the ending pound.
+		**-
+		*/
 
-        for (pProbe = pIn; *pProbe && *pProbe != L'#'; pProbe++);
+		for (pProbe = pIn; *pProbe && *pProbe != L'#'; pProbe++);
 
-        /*
-        **+
-        **  Non-pound ending found, copy pound and continue.
-        **-
-        */
+		/*
+		**+
+		**  Non-pound ending found, copy pound and continue.
+		**-
+		*/
 
-        if (*pProbe == NULL)
-        {
-            *pWork++ = L'#';
-            *pWork++ = *pIn;
-            nMaxLen -= 2;
-            continue;
-        }
+		if (*pProbe == NULL)
+		{
+			*pWork++ = L'#';
+			*pWork++ = *pIn;
+			nMaxLen -= 2;
+			continue;
+		}
 
 
-        nFmtLen = (int)(pProbe - pIn);
-        pFmt = (char*)malloc((nFmtLen * sizeof(char)) + sizeof(char));
-        if (!pFmt)
-            break;
-        wmemcpy((wchar_t*)pFmt, (const wchar_t*)pIn, nFmtLen);
-        pFmt[nFmtLen] = L'\0';
-        //LOGIF(VERB, DBG, L"Passing %s to wcsftime", pFmt);
-        nWork = (int)wcsftime((wchar_t*)pWork, nMaxLen, (const wchar_t*)pFmt, pToday);
-        //LOGIF(VERB, DBG, L"Back from wcsftime");
-        free(pFmt);
-        if (nWork == 0)
-        {
-            int nErrNum = errno;
-            if (nErrNum == EINVAL)
-                return -1;
-        }
-        if (nWork)
-            pWork += nWork;
-        else
-            *pWork = 0;
-        pIn += nFmtLen;
-        nMaxLen -= nWork;
-    }
+		nFmtLen = (int)(pProbe - pIn);              //pProbe = #.dat pIn = %m%d%Y#.dat   nFmtLen = 6
+		pFmt = (char*)malloc((nFmtLen * sizeof(char)) + sizeof(char));
+		if (!pFmt)
+			break;
+		memcpy((char*)pFmt, (const char*)pIn, nFmtLen);
+		pFmt[nFmtLen] = '\0';                      // %m%d%Y
+		//LOGIF(VERB, DBG, L"Passing %s to wcsftime", pFmt);
+		nWork = (int)strftime((char*)pWork, nMaxLen, (const char*)pFmt, pToday);
+		//LOGIF(VERB, DBG, L"Back from wcsftime");
+		free(pFmt);
+		if (nWork == 0)
+		{
+			int nErrNum = errno;
+			if (nErrNum == EINVAL)
+				return -1;
+		}
+		if (nWork)
+			pWork += nWork;
+		else
+			*pWork = 0;
+		pIn += nFmtLen;
+		nMaxLen -= nWork;
+	}
 
-    *pWork = L'\0';
-    return 0;
+	*pWork = L'\0';
+	return 0;
 }
